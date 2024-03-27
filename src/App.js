e6eb49de79690f7beb353a1504283862b06ac700
ganishm <ganishronl@gmail.com>
2024-03-27 10:13:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,175 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+//images
+import searchIcon from "./assets/search.png";
+import clearIcon from "./assets/clear.png";
+import cloudIcon from "./assets/cloud.png";
+import drizzleIcon from "./assets/drizzle.png";
+import humidityIcon from "./assets/humidity.png";
+import rainIcon from "./assets/rain.png";
+import snowIcon from "./assets/snow.png";
+import windIcon from "./assets/wind.png";
+import { useEffect, useState } from "react";
+
+const WeatherDeatils = ({
+  icon,
+  temp,
+  city,
+  country,
+  lat,
+  lon,
+  humidity,
+  wind,
+}) => {
+  return (
+    <>
+      <div className="image">
+        <img src={icon} alt="Image" />
+      </div>
+      <div className="temp">{temp}°C</div>
+      <div className="location">{city}</div>
+      <div className="country">{country}</div>
+      <div className="cord">
+        <div>
+          <span className="lat">Latitude</span>
+          <span>{lat}</span>
+        </div>
+        <div>
+          <span className="lon">Longitude</span>
+          <span>{lon}</span>
+        </div>
+      </div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidityIcon} alt="humidity" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">{humidity}%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={windIcon} alt="wind" className="icon" />
+          <div className="data">
+            <div className="wind-percent">{wind} km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 function App() {
+  const [icon, setIcon] = useState(clearIcon);
+  const [temp, setTemp] = useState(0);
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [wind, setWind] = useState(0);
+  const [text, setText] = useState("Trichy");
+  const [cityNotFound, setCityNotFound] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const weatherIconMap = {
+    "01d": clearIcon,
+    "01n": clearIcon,
+    "02d": cloudIcon,
+    "02n": cloudIcon,
+    "03d": drizzleIcon,
+    "03n": drizzleIcon,
+    "04d": drizzleIcon,
+    "04n": drizzleIcon,
+    "09d": rainIcon,
+    "09n": rainIcon,
+    "10d": rainIcon,
+    "10n": rainIcon,
+    "13d": snowIcon,
+    "13n": snowIcon,
+  };
+
+  const search = async () => {
+    setLoading(true);
+    let api_key = "b045049f5b7da0a403f923fa6847ed7d";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${api_key}&units=Metric`;
+    try {
+      let res = await fetch(url);
+      let data = await res.json();
+      if (data.cod === "404") {
+        console.error("City Not Found");
+        setCityNotFound(true);
+        setLoading(false);
+        return;
+      }
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setTemp(Math.floor(data.main.temp));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setLat(data.coord.lat);
+      setLon(data.coord.lon);
+      const weatherIconCode = data.weather[0].icon;
+      setIcon(weatherIconMap[weatherIconCode] || clearIcon);
+      setCityNotFound(false);
+    } catch (error) {
+      console.log("An error occurred:", error.message);
+      setError("An occurred while fetching weather data.");
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handleCity = (e) => {
+    setText(e.target.value);
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  useEffect(function () {
+    search();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className="container">
+        <div className="input-container">
+          <input
+            type="text"
+            className="cityInput"
+            placeholder="Search City"
+            onChange={handleCity}
+            onKeyDown={handleKeyDown}
+          />
+          <div className="search-icon" onClick={() => search()}>
+            <img src={searchIcon} alt="Search" />
+          </div>
+        </div>
+
+        {loading && <div className="loading-message">Loading...</div>}
+        {error && <div className="error-message">{error}</div>}
+        {cityNotFound && <div className="city-not-found">City Not Found</div>}
+
+        {!loading && !cityNotFound && (
+          <WeatherDeatils
+            icon={icon}
+            temp={temp}
+            city={city}
+            country={country}
+            lat={lat}
+            lon={lon}
+            humidity={humidity}
+            wind={wind}
+          />
+        )}
+        <p className="copyright">
+          Designed By <span>Ganishkar</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
